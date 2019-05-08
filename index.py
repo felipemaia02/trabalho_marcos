4396b9df3bbc239b0678cd8a2d40403901b6cdd6
Felipe Maia <felipeoliveiramaia3@gmail.com>
2019-05-08 16:45:44 -0300

    atualizado

--- index.py
+++ index.py
@@ -7,11 +7,32 @@ mysql = MySQL()
 
 mysql.init_app(app)
 
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'pcgo2013'
+app.config['MYSQL_DATABASE_DB'] = 'contatos'
+
 @app.route('/')
 def first_pg():
     return render_template('index.html')
-@app.route('/login')
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login_pg():
-    return render_template('login_page.html')
+    if request.method == 'POST':
+        login = request.form.get('login')
+        senha = request.form.get('senha')
+
+        cursor = mysql.get_db().cursor()
+
+        idlogin = get_idlogin(cursor, login, senha)
+
+        if idlogin == None:
+            return render_template('login_page.html', erro = 'Login/senha incorretos!')
+
+        else:
+            cursor.mysql.get_db().cursor()
+            return render_template('')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
